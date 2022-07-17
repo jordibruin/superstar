69444c28fc9e1a751ce3d38253f08bd8120804c2
Jordi Bruin <jordi@goodsnooze.com>
2022-07-17 22:47:58 +0200

    added project

---
+++ Superstar/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Jordi Bruin on 17/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Superstar/Shared/SuperstarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperstarApp.swift
+//  Shared
+//
+//  Created by Jordi Bruin on 17/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperstarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
